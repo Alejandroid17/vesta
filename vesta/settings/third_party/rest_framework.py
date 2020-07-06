8ac818703062cefb9a5e1a7859cfe6d138260209
Alejandro <alejandrofernandezm94@gmail.com>
2020-07-06 19:06:10 +0200

    chore: add rest_framework settings

---
+++ vesta/settings/third_party/rest_framework.py
@@ -0,0 +1,10 @@
+# Rest framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
