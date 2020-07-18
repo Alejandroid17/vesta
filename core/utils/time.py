b3d41d7f9e823aa33f12c03d9196ad298b7fadf0
Alejandro <alejandrofernandezm94@gmail.com>
2020-07-18 22:08:26 +0200

    chore: new module 'core utils time' with seasons

---
+++ core/utils/time.py
@@ -0,0 +1,9 @@
+from django.utils.translation import gettext_lazy as _
+
+SEASONS = [
+    ('NO_SEASON', _('No season')),
+    ('SPRING', _('Spring')),
+    ('SUMMER', _('Summer')),
+    ('AUTUMN', _('Autumn')),
+    ('WINTER', _('Winter'))
+]
